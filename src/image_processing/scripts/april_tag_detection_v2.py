6eabd3ad73ecc9dc7edec8157fe68568663f0c3c
Ethan <ethan.allan@strath.ac.uk>
2025-05-09 16:33:33 +0100

    Working ROS package for camera feed

---
+++ src/image_processing/scripts/april_tag_detection_v2.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+import cv2
+import numpy
+
+from cv_bridge import CvBridge, CvBridgeError
+
+
+def	callback(data, bridge):
+	rospy.loginfo("I recieved an image")
+
+	try:
+		cv_image = bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
+		cv2.imshow("Image window", cv_image)
+		cv2.waitKey(3)
+	except CvBridgeError as e:
+		print(e)
+
+	
+
+	
+def listener():
+	rospy.init_node('april_tag_detection', anonymous=True)
+
+	bridge = CvBridge()
+	rospy.Subscriber("camera/color/image_raw", Image ,callback, bridge)
+	rospy.spin()
+
+	
+if __name__ == '__main__':
+	listener()
